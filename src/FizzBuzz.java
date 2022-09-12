193e692e8daae08c1dd2356034b83f4abcd61948
Shiina Kaneko <112843281+shiinaknk@users.noreply.github.com>
2022-09-12 15:51:10 +0900

    課題再提出

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,20 +1,20 @@
 
 public class FizzBuzz {
 	public static void main(String[] args) {
-	  for(int number=1;number<=100;number++) {
+	  for(int i=1;i<=100;i++) {
 		  
-		  if(number%3==0) {
+		  if(i%3==0&i%5==0) {
+			  System.out.println("FizzBuzz");
+			  }
+		  else if(i%3==0) {
 		  System.out.println("Fizz");
 		  }
-		  if(number%5==0) {
+		  else if(i%5==0) {
 		  System.out.println("Buzz");
-		  }
-		  if(number%3==0&&number%5==0) {
-		  System.out.println("FizzBuzz");
 		  }
 		  
 		  else {
-		  System.out.println(number);
+		  System.out.println(i);
 		  }	 
 		  
 		  }
